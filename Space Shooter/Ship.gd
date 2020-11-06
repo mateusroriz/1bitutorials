24ea1e58a1ba3208c38243bb4812acb4a46621a7
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-11-06 20:34:17 -0300

    adding space shooter game and adding player, enemies, collisions and score

---
+++ Space Shooter/Ship.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+const Bullet = preload("res://Bullet.tscn") #getting acess to the bullet scene with a packed scene
+const ExplosionEffect = preload("res://ExplosionEffect.tscn")
+
+export(int) var SPEED = 100
+
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		position.y -= SPEED * delta
+	if Input.is_action_pressed("ui_down"):
+			position.y += SPEED * delta
+	if Input.is_action_just_pressed("ui_accept"):
+		fire_bullet()
+
+func fire_bullet():
+	var bullet = Bullet.instance() #instancing the bullet
+	var main = get_tree().current_scene 
+	main.add_child(bullet) #adding bullet to the child of the node
+	bullet.global_position = global_position
+
+
+func _on_Ship_area_entered(area):
+	area.queue_free()
+	queue_free()
+
+
+func _exit_tree():
+	var main = get_tree().current_scene
+	var explosionEffect = ExplosionEffect.instance()
+	main.add_child(explosionEffect)
+	explosionEffect.global_position = global_position
+
