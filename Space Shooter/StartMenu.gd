14c0c4d14dc8b0ef940ecac22246c87067cd3d02
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-11-09 23:53:32 -0300

    adding pixel font and start menu for space shooter

---
+++ Space Shooter/StartMenu.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://World.tscn")
--- Space Shooter/World.gd
+++ Space Shooter/World.gd
@@ -6,7 +6,7 @@ onready var scoreLabel = $ScoreLabel
 
 func set_score(value):
 	score = value
-	scoreLabel.text = "Score=" + str(score) 
+	scoreLabel.text = "Score= " + str(score) 
 
 #func _on_Enemy_score_up():
 #	self.score +=1 #needs self because to avoid looping
