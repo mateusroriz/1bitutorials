61b73dd8dbb7f1b75901b9631ac8dd0a10dac78c
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-11-13 19:13:20 -0300

    Fixing slope jump bug

--- Metroidvania/Player/Player.gd
+++ Metroidvania/Player/Player.gd
@@ -1,22 +1,21 @@
 extends KinematicBody2D
 
-export (int) var ACCELERATION =512
+export (int) var ACCELERATION = 512
 export (int) var MAX_SPEED = 64
 export (float) var FRICTION = 0.25
 export (int) var GRAVITY = 200
 export (int) var JUMP_FORCE = 128
 export (int) var MAX_SLOPE_ANGLE = 46
 
-var motion  = Vector2.ZERO
+var motion = Vector2.ZERO
 var snap_vector = Vector2.ZERO
 var just_jumped = false
 
 onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
 
-
 func _physics_process(delta):
-	var just_jumped = false
+	just_jumped = false
 	var input_vector = get_input_vector()
 	apply_horizontal_force(input_vector, delta)
 	apply_friction(input_vector)
@@ -26,19 +25,18 @@ func _physics_process(delta):
 	update_animations(input_vector)
 	move()
 
-
 func get_input_vector():
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	return input_vector
 
 func apply_horizontal_force(input_vector, delta):
-	if input_vector.x !=0:
-		motion.x += input_vector.x * ACCELERATION *delta
+	if input_vector.x != 0:
+		motion.x += input_vector.x * ACCELERATION * delta
 		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
 
 func apply_friction(input_vector):
-	if input_vector.x == 0 and is_on_floor(): #no buttons pressed
+	if input_vector.x == 0 and is_on_floor():
 		motion.x = lerp(motion.x, 0, FRICTION)
 
 func update_snap_vector():
@@ -50,18 +48,18 @@ func jump_check():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_FORCE
 			just_jumped = true
-			snap_vector = Vector2.ZERO # if we are jumping snap vector becomes zero
+			snap_vector = Vector2.ZERO
 	else:
-		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2: #this makes sure the character is still going up and can't double jump
-			motion.y = -JUMP_FORCE/2 #cutting the jump force
-		
+		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE/2;
+
 func apply_gravity(delta):
 	if not is_on_floor():
 		motion.y += GRAVITY * delta
-		motion.y = min(motion.y, JUMP_FORCE) #this prevents so we can't fall faster than the jump force
+		motion.y = min(motion.y, JUMP_FORCE)
 
 func update_animations(input_vector):
-	if input_vector.x !=0:
+	if input_vector.x != 0:
 		sprite.scale.x = sign(input_vector.x)
 		spriteAnimator.play("Run")
 	else:
@@ -70,24 +68,22 @@ func update_animations(input_vector):
 	if not is_on_floor():
 		spriteAnimator.play("Jump")
 
-
 func move():
 	var was_in_air = not is_on_floor()
 	var was_on_floor = is_on_floor()
 	var last_motion = motion
 	var last_position = position
 	
-	motion = move_and_slide_with_snap(motion,snap_vector *4 ,Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE)) #passing vector 2 tells where the floor is facing
-	
-	#landing
+	motion = move_and_slide_with_snap(motion, snap_vector*4, Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE))
+	# Landing
 	if was_in_air and is_on_floor():
 		motion.x = last_motion.x
 	
-	#just left ground
+	# Just left ground
 	if was_on_floor and not is_on_floor() and not just_jumped:
-		motion .y = 0
+		motion.y = 0
 		position.y = last_position.y
 	
-	# Prevent sliding (hack)
-	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) <1:
+	# Prevent Sliding (hack)
+	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) < 1:
 		position.x = last_position.x
