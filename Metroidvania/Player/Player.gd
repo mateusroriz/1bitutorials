fd34b082af5f5357c80a179316ffc05a29705706
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-11-13 19:12:33 -0300

    fixing slope movement

--- Metroidvania/Player/Player.gd
+++ Metroidvania/Player/Player.gd
@@ -9,12 +9,14 @@ export (int) var MAX_SLOPE_ANGLE = 46
 
 var motion  = Vector2.ZERO
 var snap_vector = Vector2.ZERO
+var just_jumped = false
 
 onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
 
 
 func _physics_process(delta):
+	var just_jumped = false
 	var input_vector = get_input_vector()
 	apply_horizontal_force(input_vector, delta)
 	apply_friction(input_vector)
@@ -47,6 +49,7 @@ func jump_check():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_FORCE
+			just_jumped = true
 			snap_vector = Vector2.ZERO # if we are jumping snap vector becomes zero
 	else:
 		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2: #this makes sure the character is still going up and can't double jump
@@ -69,4 +72,22 @@ func update_animations(input_vector):
 
 
 func move():
+	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var last_motion = motion
+	var last_position = position
+	
 	motion = move_and_slide_with_snap(motion,snap_vector *4 ,Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE)) #passing vector 2 tells where the floor is facing
+	
+	#landing
+	if was_in_air and is_on_floor():
+		motion.x = last_motion.x
+	
+	#just left ground
+	if was_on_floor and not is_on_floor() and not just_jumped:
+		motion .y = 0
+		position.y = last_position.y
+	
+	# Prevent sliding (hack)
+	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) <1:
+		position.x = last_position.x
