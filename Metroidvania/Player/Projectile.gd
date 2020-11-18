70b29b8b7232167c1c4ef8639e24a6496573b96e
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-11-18 00:50:28 -0300

    adding jumping visual effect and projectile explosion effect

--- Metroidvania/Player/Projectile.gd
+++ Metroidvania/Player/Projectile.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const ExplosionEffect = preload("res://Effects/ExplosionEffect.tscn")
+
 var velocity = Vector2.ZERO
 
 func _process(delta):
@@ -12,8 +14,10 @@ func _on_VisibilityNotifier2D_viewport_exited(viewport):
 
 
 func _on_Hitbox_body_entered(body): #destroying it when it hits the world
+	Utils.instance_scene_on_main(ExplosionEffect,global_position)
 	queue_free()
 
 
 func _on_Hitbox_area_entered(area): #destroying it when it hits a enemy
+	Utils.instance_scene_on_main(ExplosionEffect,global_position)
 	queue_free()
