f4b303b6bf36b643d2c9e75e2e8209d057574314
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-11-18 19:03:45 -0300

    adding stats to enemy parent for health and ignoring warning

--- Metroidvania/Player/Projectile.gd
+++ Metroidvania/Player/Projectile.gd
@@ -13,11 +13,13 @@ func _on_VisibilityNotifier2D_viewport_exited(viewport):
 	queue_free()
 
 
+# warning-ignore:unused_argument
 func _on_Hitbox_body_entered(body): #destroying it when it hits the world
 	Utils.instance_scene_on_main(ExplosionEffect,global_position)
 	queue_free()
 
 
+# warning-ignore:unused_argument
 func _on_Hitbox_area_entered(area): #destroying it when it hits a enemy
 	Utils.instance_scene_on_main(ExplosionEffect,global_position)
 	queue_free()
