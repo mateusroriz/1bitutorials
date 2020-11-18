597ddde4a94fdd941b020e4c672927a60114c71b
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-11-18 00:38:51 -0300

    adding hurtboxes and hitboxes for the bullets and enemies

--- Metroidvania/Player/Projectile.gd
+++ Metroidvania/Player/Projectile.gd
@@ -9,3 +9,11 @@ func _process(delta):
 # warning-ignore:unused_argument
 func _on_VisibilityNotifier2D_viewport_exited(viewport):
 	queue_free()
+
+
+func _on_Hitbox_body_entered(body): #destroying it when it hits the world
+	queue_free()
+
+
+func _on_Hitbox_area_entered(area): #destroying it when it hits a enemy
+	queue_free()
