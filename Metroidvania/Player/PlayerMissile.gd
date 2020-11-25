f2a057059192974772202faa88d8d8ea314964dc
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-11-25 02:14:12 -0300

    adding brick as destroyable scenario for player missiles

---
+++ Metroidvania/Player/PlayerMissile.gd
@@ -0,0 +1,12 @@
+extends "res://Player/Projectile.gd"
+
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
+const BRICK_LAYER_BIT = 4 
+
+func _on_Hitbox_body_entered(body): 
+	if body.get_collision_layer_bit(BRICK_LAYER_BIT): #is the body on this collision layer return true or false
+		body.queue_free() #if its a brick destroy it
+		Utils.instance_scene_on_main(EnemyDeathEffect, body.global_position + Vector2(8,8)) #adding v2 8,8 to put the effect in the scenter of the sprites
+	._on_Hitbox_body_entered(body) #running the logic inside projectile
+
